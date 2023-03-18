a1803ab1b221a435801c55055d67beb6fc796163
aaronbiscotti <thesoupmanguy@gmail.com>
2023-03-18 13:39:03 -0400

    inity

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -4,5 +4,11 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const clientCredentials = {
-    apiKey: ""
+    apiKey: "AIzaSyCtd-fTecxT4KGjI-3hQ79fbAEy-sGdfm0",
+    authDomain: "medea-7ef09.firebaseapp.com",
+    projectId: "medea-7ef09",
+    storageBucket: "medea-7ef09.appspot.com",
+    messagingSenderId: "200117037823",
+    appId: "1:200117037823:web:7579feffdc61ed53c2baca",
+    measurementId: "G-3539M05PFZ"
 }
